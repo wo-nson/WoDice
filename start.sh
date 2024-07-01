693b56eceecd258469a82bb7abffc7fca1f5107f
wo-nson <lots.of.wokura@gmail.com>
2024-07-01 15:29:12 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-#DISCORD_BOT_TOKEN=
-BCDICE_API_URL=javascript:onclick=toClipBoard("https://bcdice.onlinesession.app");
+DISCORD_BOT_TOKEN={token}
+BCDICE_API_URL=https://bcdice.onlinesession.app
+BCDICE_API_SECONDARY=https://bcdice.trpg.net
 IGNORE_ERROR=
 # BCDICE_PASSWORD=PleaseChangeMeIfYouUseThis
 # BCDICE_API_SECONDARY=http://secondary.bcdice-api.yourdomain.co.jp/
 # BCDICE_DEFAULT_SYSTEM=DiceBot
-# BCDICE_MENTION_MODE=1
+BCDICE_MENTION_MODE=1
 
 java -jar discord-bcdicebot.jar "$DISCORD_BOT_TOKEN" "$BCDICE_API_URL" "$IGNORE_ERROR"
